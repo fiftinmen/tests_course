59bff6200927a7fd5552d3df8659b7814b6c25e0
fiftinmen <fiftin.men@gmail.com>
2024-03-24 14:37:57 +0300

    replace if tests to assert tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,20 +1,23 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello', 'Функция работает неверно!'
+
+
+assert capitalize(',') == ',', 'Функция работает неверно!'
+
+
 try:
-    if capitalize(None) is not None:
-        raise Exception('Функция работает неверно!')
+    assert capitalize(None) is None, 'Функция работает неверно!'
 except:
     raise Exception('Функция работает неверно!')
 
-if capitalize('1') != '1':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('_') != '_':
-    raise Exception('Функция работает неверно!')
+assert capitalize('1') == '1', 'Функция работает неверно!'
+
+
+assert capitalize('_') == '_', 'Функция работает неверно!'
+
 
 print('Все тесты пройдены!')
+
